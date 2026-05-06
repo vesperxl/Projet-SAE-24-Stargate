782ca1d12f1f865b1f5a827ca0f9cf2291297947
louis stenger <louisstenger07@gmail.com>
2026-05-06 11:03:47 +0200

    ajout de la connexion et du ds global

---
+++ Connexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_SAE_24_Stargate
+{
+    internal class Connexion
+    {
+        private static SQLiteConnection connec;
+
+        private Connexion() { }
+
+        public static SQLiteConnection Connec
+        {
+            get
+            {
+                if (connec == null) {
+                    try
+                    {
+                        string chaine = @"Data Source = Stargate.bd";
+                        connec = new SQLiteConnection(chaine);
+                        connec.Open();
+                    }
+                    catch(SQLiteException err)
+                    {
+                        Console.WriteLine("Erreur lors de l'ouverture de la connexion");
+                    }
+                }
+
+                return connec;
+            }
+          
+        }
+    }
+}
