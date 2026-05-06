0d18e139ff40f8fb8aa3bde642b0a64a1b983eab
louis stenger <louisstenger07@gmail.com>
2026-05-06 11:44:10 +0200

    ajout frmDemarrage

---
+++ frmDemarrage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet_SAE_24_Stargate
+{
+    public partial class frmDemarrage : Form
+    {
+        public frmDemarrage()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDemarrage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
