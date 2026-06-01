c794620c020e6ac5cf86cbfcf4ea08bcd272bb4d
Lilian Despres <despreslilian@gmail.com>
2026-06-01 14:37:06 +0200

    comiteuh

--- ucRaces.cs
+++ ucRaces.cs
@@ -22,7 +22,7 @@ namespace Projet_SAE_24_Stargate
             InitializeComponent();
             lblNom.Text = nom;
             lblOrigine.Text = origine;
-            BackColor = Color.FromName(couleur);
+            panel1.BackColor = Color.FromName(couleur);
             picBox.Image = image;
             picBox.SizeMode = PictureBoxSizeMode.Zoom;
             if (races == 0)
