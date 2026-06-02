828a38ffadf990fb9dbbd75c98fd5037719f80f1
Lilian Despres <despreslilian@gmail.com>
2026-06-02 13:37:47 +0200

    comiteuh

--- ucRaces.cs
+++ ucRaces.cs
@@ -24,7 +24,6 @@ namespace Projet_SAE_24_Stargate
             lblOrigine.Text = origine;
             panel1.BackColor = Color.FromName(couleur);
             picBox.Image = image;
-            picBox.SizeMode = PictureBoxSizeMode.Zoom;
             if (races == 0)
             {
                 grpBoxInsArm.Text = "Instrument";
