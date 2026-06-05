52e66002a6708e5323270ab9b8d426eb77a5e3dc
Lilian Despres <despreslilian@gmail.com>
2026-06-05 13:39:50 +0200

    normalement commit final de moi (pas sur tkt bg) bisous bisous

--- FrmRaces.Designer.cs
+++ FrmRaces.Designer.cs
@@ -50,20 +50,18 @@
             // 
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(275, 8);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(412, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(884, 508);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1326, 782);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // chkBoxTriAlpha
             // 
             this.chkBoxTriAlpha.AutoSize = true;
             this.chkBoxTriAlpha.ForeColor = System.Drawing.Color.White;
-            this.chkBoxTriAlpha.Location = new System.Drawing.Point(4, 31);
-            this.chkBoxTriAlpha.Margin = new System.Windows.Forms.Padding(2);
+            this.chkBoxTriAlpha.Location = new System.Drawing.Point(6, 48);
             this.chkBoxTriAlpha.Name = "chkBoxTriAlpha";
-            this.chkBoxTriAlpha.Size = new System.Drawing.Size(159, 24);
+            this.chkBoxTriAlpha.Size = new System.Drawing.Size(233, 33);
             this.chkBoxTriAlpha.TabIndex = 1;
             this.chkBoxTriAlpha.Text = "Tri Alphabétique";
             this.chkBoxTriAlpha.UseVisualStyleBackColor = true;
@@ -73,10 +71,9 @@
             // 
             this.chkBoxTriCoul.AutoSize = true;
             this.chkBoxTriCoul.ForeColor = System.Drawing.Color.White;
-            this.chkBoxTriCoul.Location = new System.Drawing.Point(4, 59);
-            this.chkBoxTriCoul.Margin = new System.Windows.Forms.Padding(2);
+            this.chkBoxTriCoul.Location = new System.Drawing.Point(6, 91);
             this.chkBoxTriCoul.Name = "chkBoxTriCoul";
-            this.chkBoxTriCoul.Size = new System.Drawing.Size(115, 24);
+            this.chkBoxTriCoul.Size = new System.Drawing.Size(171, 33);
             this.chkBoxTriCoul.TabIndex = 2;
             this.chkBoxTriCoul.Text = "Tri Couleur";
             this.chkBoxTriCoul.UseVisualStyleBackColor = true;
@@ -86,10 +83,9 @@
             // 
             this.cboNom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboNom.FormattingEnabled = true;
-            this.cboNom.Location = new System.Drawing.Point(6, 218);
-            this.cboNom.Margin = new System.Windows.Forms.Padding(2);
+            this.cboNom.Location = new System.Drawing.Point(9, 335);
             this.cboNom.Name = "cboNom";
-            this.cboNom.Size = new System.Drawing.Size(246, 32);
+            this.cboNom.Size = new System.Drawing.Size(367, 41);
             this.cboNom.TabIndex = 3;
             this.cboNom.SelectedIndexChanged += new System.EventHandler(this.cboNom_SelectedIndexChanged);
             // 
@@ -97,10 +93,9 @@
             // 
             this.cboColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboColor.FormattingEnabled = true;
-            this.cboColor.Location = new System.Drawing.Point(5, 138);
-            this.cboColor.Margin = new System.Windows.Forms.Padding(2);
+            this.cboColor.Location = new System.Drawing.Point(8, 212);
             this.cboColor.Name = "cboColor";
-            this.cboColor.Size = new System.Drawing.Size(247, 32);
+            this.cboColor.Size = new System.Drawing.Size(368, 41);
             this.cboColor.TabIndex = 4;
             this.cboColor.SelectedIndexChanged += new System.EventHandler(this.cboColor_SelectedIndexChanged);
             // 
@@ -108,10 +103,9 @@
             // 
             this.cboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboType.FormattingEnabled = true;
-            this.cboType.Location = new System.Drawing.Point(5, 57);
-            this.cboType.Margin = new System.Windows.Forms.Padding(2);
+            this.cboType.Location = new System.Drawing.Point(8, 88);
             this.cboType.Name = "cboType";
-            this.cboType.Size = new System.Drawing.Size(247, 32);
+            this.cboType.Size = new System.Drawing.Size(368, 41);
             this.cboType.TabIndex = 5;
             this.cboType.SelectedIndexChanged += new System.EventHandler(this.cboType_SelectedIndexChanged);
             // 
@@ -120,11 +114,9 @@
             this.groupBox1.Controls.Add(this.chkBoxTriAlpha);
             this.groupBox1.Controls.Add(this.chkBoxTriCoul);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(8, 8);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(258, 91);
+            this.groupBox1.Size = new System.Drawing.Size(387, 140);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Tri";
@@ -138,10 +130,10 @@
             this.groupBox2.Controls.Add(this.cboType);
             this.groupBox2.Controls.Add(this.cboNom);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(9, 104);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Location = new System.Drawing.Point(14, 160);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(257, 284);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(386, 437);
             this.groupBox2.TabIndex = 7;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Filtre";
@@ -149,39 +141,37 @@
             // lblNom
             // 
             this.lblNom.AutoSize = true;
-            this.lblNom.Location = new System.Drawing.Point(5, 192);
-            this.lblNom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNom.Location = new System.Drawing.Point(8, 295);
             this.lblNom.Name = "lblNom";
-            this.lblNom.Size = new System.Drawing.Size(54, 24);
+            this.lblNom.Size = new System.Drawing.Size(80, 33);
             this.lblNom.TabIndex = 8;
             this.lblNom.Text = "Nom";
             // 
             // lblColor
             // 
             this.lblColor.AutoSize = true;
-            this.lblColor.Location = new System.Drawing.Point(2, 112);
-            this.lblColor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblColor.Location = new System.Drawing.Point(3, 172);
             this.lblColor.Name = "lblColor";
-            this.lblColor.Size = new System.Drawing.Size(84, 24);
+            this.lblColor.Size = new System.Drawing.Size(124, 33);
             this.lblColor.TabIndex = 7;
             this.lblColor.Text = "Couleur";
             // 
             // lblHosti
             // 
             this.lblHosti.AutoSize = true;
-            this.lblHosti.Location = new System.Drawing.Point(4, 31);
-            this.lblHosti.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblHosti.Location = new System.Drawing.Point(6, 48);
             this.lblHosti.Name = "lblHosti";
-            this.lblHosti.Size = new System.Drawing.Size(84, 24);
+            this.lblHosti.Size = new System.Drawing.Size(129, 33);
             this.lblHosti.TabIndex = 6;
             this.lblHosti.Text = "Hostilité";
             // 
             // btnRes
             // 
             this.btnRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnRes.Location = new System.Drawing.Point(137, 393);
+            this.btnRes.Location = new System.Drawing.Point(206, 605);
+            this.btnRes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnRes.Name = "btnRes";
-            this.btnRes.Size = new System.Drawing.Size(129, 122);
+            this.btnRes.Size = new System.Drawing.Size(194, 188);
             this.btnRes.TabIndex = 8;
             this.btnRes.Text = "Réinitialiser";
             this.btnRes.UseVisualStyleBackColor = true;
@@ -192,9 +182,10 @@
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = global::Projet_SAE_24_Stargate.Properties.Resources.homeNeon;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 393);
+            this.pictureBox1.Location = new System.Drawing.Point(18, 605);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(122, 122);
+            this.pictureBox1.Size = new System.Drawing.Size(182, 187);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
@@ -202,16 +193,15 @@
             // 
             // frmRaces
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(1168, 527);
+            this.ClientSize = new System.Drawing.Size(1752, 811);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnRes);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmRaces";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Liste Des Races";
--- frmDemarrage.Designer.cs
+++ frmDemarrage.Designer.cs
@@ -65,9 +65,10 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = global::Projet_SAE_24_Stargate.Properties.Resources.refresh;
-            this.pictureBox1.Location = new System.Drawing.Point(591, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(1611, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(59, 54);
+            this.pictureBox1.Size = new System.Drawing.Size(122, 103);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -80,18 +81,20 @@
             this.flpMission.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.flpMission.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.flpMission.Controls.Add(this.chkEnCours);
-            this.flpMission.Location = new System.Drawing.Point(656, 12);
+            this.flpMission.Location = new System.Drawing.Point(1741, 18);
+            this.flpMission.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flpMission.Name = "flpMission";
-            this.flpMission.Padding = new System.Windows.Forms.Padding(10);
-            this.flpMission.Size = new System.Drawing.Size(533, 687);
+            this.flpMission.Padding = new System.Windows.Forms.Padding(15, 15, 15, 15);
+            this.flpMission.Size = new System.Drawing.Size(810, 1335);
             this.flpMission.TabIndex = 1;
             // 
             // chkEnCours
             // 
-            this.chkEnCours.Font = new System.Drawing.Font("Inter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkEnCours.Location = new System.Drawing.Point(13, 13);
+            this.chkEnCours.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkEnCours.Location = new System.Drawing.Point(19, 20);
+            this.chkEnCours.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.chkEnCours.Name = "chkEnCours";
-            this.chkEnCours.Size = new System.Drawing.Size(500, 22);
+            this.chkEnCours.Size = new System.Drawing.Size(750, 34);
             this.chkEnCours.TabIndex = 1;
             this.chkEnCours.Text = "Afficher seulement les missions en cours";
             this.chkEnCours.UseVisualStyleBackColor = true;
@@ -101,21 +104,23 @@
             // 
             this.lblTitre.AutoSize = true;
             this.lblTitre.BackColor = System.Drawing.Color.Transparent;
-            this.lblTitre.Font = new System.Drawing.Font("Modern No. 20", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitre.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitre.ForeColor = System.Drawing.Color.Red;
-            this.lblTitre.Location = new System.Drawing.Point(35, 51);
+            this.lblTitre.Location = new System.Drawing.Point(52, 78);
+            this.lblTitre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitre.Name = "lblTitre";
-            this.lblTitre.Size = new System.Drawing.Size(515, 37);
+            this.lblTitre.Size = new System.Drawing.Size(796, 63);
             this.lblTitre.TabIndex = 3;
             this.lblTitre.Tag = "title";
             this.lblTitre.Text = "STARGATE COMMAND USAF";
             // 
             // lblRace
             // 
-            this.lblRace.Font = new System.Drawing.Font("Inter", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRace.Location = new System.Drawing.Point(96, 1);
+            this.lblRace.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRace.Location = new System.Drawing.Point(176, 2);
+            this.lblRace.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRace.Name = "lblRace";
-            this.lblRace.Size = new System.Drawing.Size(177, 70);
+            this.lblRace.Size = new System.Drawing.Size(387, 163);
             this.lblRace.TabIndex = 6;
             this.lblRace.Text = "Liste des races";
             this.lblRace.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -127,19 +132,21 @@
             this.flpListRace.Controls.Add(this.pictureRace);
             this.flpListRace.Controls.Add(this.lblRace);
             this.flpListRace.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.flpListRace.Location = new System.Drawing.Point(22, 293);
+            this.flpListRace.Location = new System.Drawing.Point(40, 578);
+            this.flpListRace.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flpListRace.Name = "flpListRace";
-            this.flpListRace.Padding = new System.Windows.Forms.Padding(1);
-            this.flpListRace.Size = new System.Drawing.Size(289, 72);
+            this.flpListRace.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.flpListRace.Size = new System.Drawing.Size(580, 175);
             this.flpListRace.TabIndex = 10;
             this.flpListRace.Click += new System.EventHandler(this.flpListRace_Click);
             // 
             // pictureRace
             // 
             this.pictureRace.Image = global::Projet_SAE_24_Stargate.Properties.Resources.alien;
-            this.pictureRace.Location = new System.Drawing.Point(4, 4);
+            this.pictureRace.Location = new System.Drawing.Point(6, 7);
+            this.pictureRace.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureRace.Name = "pictureRace";
-            this.pictureRace.Size = new System.Drawing.Size(86, 67);
+            this.pictureRace.Size = new System.Drawing.Size(162, 158);
             this.pictureRace.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureRace.TabIndex = 5;
             this.pictureRace.TabStop = false;
@@ -147,10 +154,11 @@
             // 
             // lblPlanete
             // 
-            this.lblPlanete.Font = new System.Drawing.Font("Inter", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPlanete.Location = new System.Drawing.Point(96, 1);
+            this.lblPlanete.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlanete.Location = new System.Drawing.Point(176, 2);
+            this.lblPlanete.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPlanete.Name = "lblPlanete";
-            this.lblPlanete.Size = new System.Drawing.Size(177, 70);
+            this.lblPlanete.Size = new System.Drawing.Size(387, 163);
             this.lblPlanete.TabIndex = 6;
             this.lblPlanete.Text = "Liste des planètes";
             this.lblPlanete.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -162,19 +170,21 @@
             this.flpListePlanete.Controls.Add(this.picturePlanete);
             this.flpListePlanete.Controls.Add(this.lblPlanete);
             this.flpListePlanete.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.flpListePlanete.Location = new System.Drawing.Point(22, 404);
+            this.flpListePlanete.Location = new System.Drawing.Point(40, 844);
+            this.flpListePlanete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flpListePlanete.Name = "flpListePlanete";
-            this.flpListePlanete.Padding = new System.Windows.Forms.Padding(1);
-            this.flpListePlanete.Size = new System.Drawing.Size(289, 72);
+            this.flpListePlanete.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.flpListePlanete.Size = new System.Drawing.Size(580, 175);
             this.flpListePlanete.TabIndex = 11;
             this.flpListePlanete.Click += new System.EventHandler(this.flpListePlanete_Click);
             // 
             // picturePlanete
             // 
             this.picturePlanete.Image = global::Projet_SAE_24_Stargate.Properties.Resources.neonPlanete;
-            this.picturePlanete.Location = new System.Drawing.Point(4, 4);
+            this.picturePlanete.Location = new System.Drawing.Point(6, 7);
+            this.picturePlanete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.picturePlanete.Name = "picturePlanete";
-            this.picturePlanete.Size = new System.Drawing.Size(86, 67);
+            this.picturePlanete.Size = new System.Drawing.Size(162, 158);
             this.picturePlanete.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picturePlanete.TabIndex = 5;
             this.picturePlanete.TabStop = false;
@@ -186,19 +196,21 @@
             this.flpNvMission.Controls.Add(this.pictureMission);
             this.flpNvMission.Controls.Add(this.lblMission);
             this.flpNvMission.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.flpNvMission.Location = new System.Drawing.Point(22, 186);
+            this.flpNvMission.Location = new System.Drawing.Point(40, 325);
+            this.flpNvMission.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flpNvMission.Name = "flpNvMission";
-            this.flpNvMission.Padding = new System.Windows.Forms.Padding(1);
-            this.flpNvMission.Size = new System.Drawing.Size(289, 72);
+            this.flpNvMission.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.flpNvMission.Size = new System.Drawing.Size(580, 175);
             this.flpNvMission.TabIndex = 9;
             this.flpNvMission.Click += new System.EventHandler(this.flpNvMission_Click);
             // 
             // pictureMission
             // 
             this.pictureMission.Image = global::Projet_SAE_24_Stargate.Properties.Resources.ajouterMission;
-            this.pictureMission.Location = new System.Drawing.Point(4, 4);
+            this.pictureMission.Location = new System.Drawing.Point(6, 7);
+            this.pictureMission.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureMission.Name = "pictureMission";
-            this.pictureMission.Size = new System.Drawing.Size(86, 67);
+            this.pictureMission.Size = new System.Drawing.Size(162, 158);
             this.pictureMission.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureMission.TabIndex = 5;
             this.pictureMission.TabStop = false;
@@ -206,10 +218,11 @@
             // 
             // lblMission
             // 
-            this.lblMission.Font = new System.Drawing.Font("Inter", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMission.Location = new System.Drawing.Point(96, 1);
+            this.lblMission.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMission.Location = new System.Drawing.Point(176, 2);
+            this.lblMission.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMission.Name = "lblMission";
-            this.lblMission.Size = new System.Drawing.Size(177, 70);
+            this.lblMission.Size = new System.Drawing.Size(387, 172);
             this.lblMission.TabIndex = 6;
             this.lblMission.Text = "Nouvelle mission";
             this.lblMission.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -221,19 +234,21 @@
             this.flpStat.Controls.Add(this.pictureStt);
             this.flpStat.Controls.Add(this.lblStat);
             this.flpStat.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.flpStat.Location = new System.Drawing.Point(22, 516);
+            this.flpStat.Location = new System.Drawing.Point(40, 1118);
+            this.flpStat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flpStat.Name = "flpStat";
-            this.flpStat.Padding = new System.Windows.Forms.Padding(1);
-            this.flpStat.Size = new System.Drawing.Size(289, 72);
+            this.flpStat.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.flpStat.Size = new System.Drawing.Size(580, 175);
             this.flpStat.TabIndex = 12;
             this.flpStat.Click += new System.EventHandler(this.flpStat_Click);
             // 
             // pictureStt
             // 
             this.pictureStt.Image = global::Projet_SAE_24_Stargate.Properties.Resources.stat;
-            this.pictureStt.Location = new System.Drawing.Point(4, 4);
+            this.pictureStt.Location = new System.Drawing.Point(6, 7);
+            this.pictureStt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureStt.Name = "pictureStt";
-            this.pictureStt.Size = new System.Drawing.Size(86, 67);
+            this.pictureStt.Size = new System.Drawing.Size(162, 158);
             this.pictureStt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureStt.TabIndex = 5;
             this.pictureStt.TabStop = false;
@@ -241,10 +256,11 @@
             // 
             // lblStat
             // 
-            this.lblStat.Font = new System.Drawing.Font("Inter", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStat.Location = new System.Drawing.Point(96, 1);
+            this.lblStat.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStat.Location = new System.Drawing.Point(176, 2);
+            this.lblStat.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStat.Name = "lblStat";
-            this.lblStat.Size = new System.Drawing.Size(177, 70);
+            this.lblStat.Size = new System.Drawing.Size(387, 163);
             this.lblStat.TabIndex = 6;
             this.lblStat.Text = "Statistiques";
             this.lblStat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -253,19 +269,20 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Projet_SAE_24_Stargate.Properties.Resources.fondTitle;
-            this.pictureBox2.Location = new System.Drawing.Point(6, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(9, 6);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(579, 147);
+            this.pictureBox2.Size = new System.Drawing.Size(887, 226);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
             // 
             // frmDemarrage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Projet_SAE_24_Stargate.Properties.Resources.backgroundDemar;
-            this.ClientSize = new System.Drawing.Size(1201, 711);
+            this.ClientSize = new System.Drawing.Size(2564, 1367);
             this.Controls.Add(this.flpNvMission);
             this.Controls.Add(this.lblTitre);
             this.Controls.Add(this.flpStat);
@@ -274,9 +291,11 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.flpMission);
             this.Controls.Add(this.pictureBox2);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmDemarrage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tableau de bord";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmDemarrage_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.flpMission.ResumeLayout(false);
