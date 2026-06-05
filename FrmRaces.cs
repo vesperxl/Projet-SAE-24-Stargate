fad51c0d589f5b9dcb6bb9a2d87297e201f2df55
Lilian Despres <despreslilian@gmail.com>
2026-06-05 12:23:14 +0200

    normalement push final

--- FrmRaces.cs
+++ FrmRaces.cs
@@ -12,15 +12,17 @@ namespace Projet_SAE_24_Stargate
 {
     public partial class frmRaces : Form
     {
+        // Variable Pour Éviter Les Mises À Jour Infini Des Combobox
         private bool enUpdate = false;
 
         public frmRaces()
         {
             InitializeComponent();
+            // Application Du Thème Visuel Au Formulaire
             ThemeCp.ApplyTheme(this);
-
         }
 
+        // Dictionnaire Pour Traduire Les Noms De Couleurs Du Français Vers L'Anglais
         Dictionary<string, string> dicoTrad = new Dictionary<string, string>()
         {
             { "Bleu", "Blue" },
@@ -33,8 +35,10 @@ namespace Projet_SAE_24_Stargate
             { "Violet", "Purple" }
         };
 
+        // Initialisation Du Formulaire Au Chargement
         private void frmRaces_Load(object sender, EventArgs e)
         {
+            // Remplissage Du Combobox Des Noms D'Espèces Depuis La Base De Données
             cboNom.Items.Add("Tous");
             foreach (DataRow row in MesDatas.DsGlobal.Tables["Espece"].Rows)
             {
@@ -42,14 +46,12 @@ namespace Projet_SAE_24_Stargate
             }
             cboNom.SelectedIndex = 0;
 
+            // Remplissage Du Combobox Des Couleurs En Évitant Les Doublons
             cboColor.Items.Add("Toutes");
-
             List<string> couleursAjoutees = new List<string>();
-
             foreach (DataRow row in MesDatas.DsGlobal.Tables["Espece"].Rows)
             {
                 string couleur = row["couleur"].ToString();
-
                 if (!couleursAjoutees.Contains(couleur))
                 {
                     couleursAjoutees.Add(couleur);
@@ -58,14 +60,17 @@ namespace Projet_SAE_24_Stargate
             }
             cboColor.SelectedIndex = 0;
 
+            // Remplissage Du Combobox Du Type D'Espèce (Alliés Ou Ennemies)
             cboType.Items.Add("Toutes");
             cboType.Items.Add("Alliés");
             cboType.Items.Add("Ennemies");
             cboType.SelectedIndex = 0;
 
+            // Affichage Initial Des Races Sans Tri
             genererRaces(false, false);
         }
 
+        // Quand La Case "Tri Alphabétique" Est Cochée, On Décoche Le Tri Par Couleur Et Recharge
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if (chkBoxTriAlpha.Checked)
@@ -75,6 +80,7 @@ namespace Projet_SAE_24_Stargate
             }
         }
 
+        // Quand La Case "Tri Par Couleur" Est Cochée, On Décoche Le Tri Alphabétique Et Recharge
         private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
         {
             if (chkBoxTriCoul.Checked)
@@ -84,25 +90,31 @@ namespace Projet_SAE_24_Stargate
             }
         }
 
+        // Méthode Principale : Génère Et Affiche Les Cartes De Races Selon Les Filtres Et Le Tri
         private void genererRaces(bool alpha, bool colo)
         {
+            // Nettoyage Du Panneau Avant De Le Remplir À Nouveau
             flowLayoutPanel1.Controls.Clear();
 
+            // Choix De L'Ordre De Tri Selon Les Cases Cochées
             string ordreTri;
-
             if (alpha)
             {
                 ordreTri = "nom ASC";
             }
+                
             else if (colo)
             {
                 ordreTri = "couleur ASC";
             }
+                
             else
             {
                 ordreTri = "id ASC";
             }
+                
 
+            // Construction Des Conditions De Filtre Selon Les Sélections Des Combobox
             List<string> conditions = new List<string>();
 
             if (cboNom.SelectedItem != null && cboNom.SelectedItem.ToString() != "Tous")
@@ -117,16 +129,19 @@ namespace Projet_SAE_24_Stargate
 
             string filtre = string.Join(" AND ", conditions);
 
+            // Récupération Du Type Sélectionné (Toutes / Alliés / Ennemies)
             string typeSelectionne = "Toutes";
             if (cboType.SelectedItem != null)
             {
                 typeSelectionne = cboType.SelectedItem.ToString();
             }
 
+            // Parcours De Chaque Espèce Filtrée Et Triée
             foreach (DataRow row in MesDatas.DsGlobal.Tables["Espece"].Select(filtre, ordreTri))
             {
                 string id = row["id"].ToString();
 
+                // Vérification Si L'Espèce Est Alliée Ou Ennemie
                 DataRow[] estAllie = MesDatas.DsGlobal.Tables["Allie"].Select("idEspece = " + id);
                 DataRow[] estEnnemi = MesDatas.DsGlobal.Tables["Ennemi"].Select("idEspece = " + id);
 
@@ -134,132 +149,118 @@ namespace Projet_SAE_24_Stargate
                 string couleur = row["couleur"].ToString();
                 string origine = "Inconnue";
 
-                string nomImage = nom + ".png";
-                string cheminImage = "./../../pic/" + nomImage;
-
+                // Chargement De L'Image Correspondant À L'Espèce Depuis Les Ressources
                 Image image = (Image)Properties.Resources.ResourceManager.GetObject(nom);
 
                 string insArme = "";
                 string attitude = "";
                 string dateContact = "";
 
+                // Recherche De La Planète D'Origine De L'Espèce
                 DataRow[] habitation = MesDatas.DsGlobal.Tables["Habiter"].Select("idEspece = " + id);
 
+                // Traduction De La Couleur En Anglais Pour L'Affichage
                 if (dicoTrad.ContainsKey(couleur))
                 {
                     couleur = dicoTrad[couleur];
                 }
+                    
 
+                // Récupération Du Nom De La Planète Si Disponible
                 if (habitation.Length > 0)
                 {
                     origine = habitation[0]["nomPlanete"].ToString();
                 }
 
+                // -1 = Neutre, 0 = Allié, 1 = Ennemi
                 int allie_enemy = -1;
 
                 if (estAllie.Length > 0)
                 {
+                    // Récupération Des Informations Spécifiques Aux Alliés
                     insArme = estAllie[0]["instrumentMusique"].ToString();
                     attitude = estAllie[0]["degreBienveillance"].ToString();
-
                     if (estAllie[0]["datePremierContact"] != DBNull.Value)
-                    {
                         dateContact = "1er Contact : " + estAllie[0]["datePremierContact"].ToString();
-                    }
                     allie_enemy = 0;
                 }
                 else if (estEnnemi.Length > 0)
                 {
+                    // Récupération Des Informations Spécifiques Aux Ennemis
                     insArme = estEnnemi[0]["typeArme"].ToString();
                     attitude = estEnnemi[0]["degreAgressivite"].ToString();
                     dateContact = "";
                     allie_enemy = 1;
                 }
 
+                // Filtrage Par Type : On Ignore Les Espèces Qui Ne Correspondent Pas Au Filtre
                 if (typeSelectionne == "Alliés" && allie_enemy != 0)
                 {
                     continue;
                 }
+
                 if (typeSelectionne == "Ennemies" && allie_enemy != 1)
                 {
                     continue;
                 }
 
+                // Création Et Ajout De La Carte Visuelle De La Race Dans Le Panneau
                 ucRaces newRaces = new ucRaces(nom, origine, couleur, image, insArme, attitude, dateContact, allie_enemy);
                 flowLayoutPanel1.Controls.Add(newRaces);
             }
         }
 
+        // Quand Le Nom Change : Réinitialise Les Autres Filtres Et Rafraîchit L'Affichage
         private void cboNom_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (!enUpdate)
             {
                 enUpdate = true;
-
-                if (cboColor.Items.Count > 0)
-                {
-                    cboColor.SelectedIndex = 0;
-                }
-
-                if (cboType.Items.Count > 0)
-                {
-                    cboType.SelectedIndex = 0;
-                }
-
+                if (cboColor.Items.Count > 0) cboColor.SelectedIndex = 0;
+                if (cboType.Items.Count > 0) cboType.SelectedIndex = 0;
                 genererRaces(chkBoxTriAlpha.Checked, chkBoxTriCoul.Checked);
-
                 enUpdate = false;
             }
         }
 
+        // Quand La Couleur Change : Réinitialise Le Filtre Par Nom Et Rafraîchit L'Affichage
         private void cboColor_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (!enUpdate)
             {
                 enUpdate = true;
-
-                if (cboNom.Items.Count > 0)
-                {
-                    cboNom.SelectedIndex = 0;
-                }
-
+                if (cboNom.Items.Count > 0) cboNom.SelectedIndex = 0;
                 genererRaces(chkBoxTriAlpha.Checked, chkBoxTriCoul.Checked);
-
                 enUpdate = false;
             }
         }
 
+        // Quand Le Type Change : Réinitialise Le Filtre Par Nom Et Rafraîchit L'Affichage
         private void cboType_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (!enUpdate)
             {
                 enUpdate = true;
-
-                if (cboNom.Items.Count > 0)
-                {
-                    cboNom.SelectedIndex = 0;
-                }
-
+                if (cboNom.Items.Count > 0) cboNom.SelectedIndex = 0;
                 genererRaces(chkBoxTriAlpha.Checked, chkBoxTriCoul.Checked);
-
                 enUpdate = false;
             }
         }
 
+        // Bouton Réinitialiser : Remet Tous Les Filtres Et Tris À Zéro
         private void btnRes_Click(object sender, EventArgs e)
         {
             enUpdate = true;
-
             chkBoxTriAlpha.Checked = false;
             chkBoxTriCoul.Checked = false;
             cboColor.SelectedIndex = 0;
             cboNom.SelectedIndex = 0;
             cboType.SelectedIndex = 0;
-
             enUpdate = false;
             genererRaces(false, false);
         }
 
+        // Clic Sur L'Image De Fermeture : Ferme Le Formulaire
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             this.Close();
--- formPlanetes.cs
+++ formPlanetes.cs
@@ -13,18 +13,22 @@ namespace Projet_SAE_24_Stargate
 {
     public partial class formPlanetes : Form
     {
+        // Chaîne De Connexion À La Base De Données SQLite
         string chcon = @"Data Source=Stargate.db";
+        // Nom De La Planète Actuellement Sélectionnée
         String planetSelect = "";
 
-        // Booléen pour empêcher les boucles d'événements
+        // Booléen Pour Empêcher Les Boucles D'Événements
         private bool enUpdate = false;
 
         public formPlanetes()
         {
             InitializeComponent();
+            // Application Du Thème Visuel Au Formulaire
             ThemeCp.ApplyTheme(this);
         }
 
+        // Dictionnaire Pour Traduire Les Noms De Couleurs Du Français Vers L'Anglais
         Dictionary<string, string> dicoTrad = new Dictionary<string, string>()
         {
             { "Bleu", "Blue" },
@@ -37,37 +41,46 @@ namespace Projet_SAE_24_Stargate
             { "Violet", "Purple" }
         };
 
+        // Initialisation Du Formulaire Au Chargement
         private void formPlanetes_Load(object sender, EventArgs e)
         {
+            // Abonnement Des Événements Aux Méthodes Correspondantes
             cboData.SelectedIndexChanged += cboData_SelectedIndexChanged;
             cboAliens.SelectedIndexChanged += cboAliens_SelectedIndexChanged; 
             chkTriAlpha.CheckedChanged += chkTriAlpha_CheckedChanged;
             ChkTriTemp.CheckedChanged += ChkTriTemp_CheckedChanged;
             chkTriPes.CheckedChanged += chkTriPes_CheckedChanged;
 
+            // Remplissage Du Combobox De Filtre Par DataBaz
             cboData.Items.Clear();
             cboData.Items.Add("Toutes les planètes");
             cboData.Items.Add("Avec DataBaz");
             cboData.Items.Add("Sans DataBaz");
 
+            // Remplissage Du Combobox De Filtre Par Présence D'Espèces
             cboAliens.Items.Clear();
             cboAliens.Items.Add("Peu importe");
             cboAliens.Items.Add("Avec espèces");
             cboAliens.Items.Add("Sans aucune espèce");
 
+            // Sélection Des Index Par Défaut Sans Déclencher Les Événements
             enUpdate = true;
             cboData.SelectedIndex = 0;
             cboAliens.SelectedIndex = 0;
             enUpdate = false;
 
+            // Affichage Initial Des Planètes Sans Tri
             genererPlanetes(false, false, false);
         }
 
+        // Méthode Principale : Génère Et Affiche Les Cartes De Planètes Selon Les Filtres Et Le Tri
         private void genererPlanetes(bool alpha, bool temp, bool pes)
         {
+            // Nettoyage Des Deux Panneaux Avant De Les Remplir À Nouveau
             flpMonstres.Controls.Clear();
             flpPlanet.Controls.Clear();
 
+            // Choix De L'Ordre De Tri Selon Les Cases Cochées
             string ordreTri = "";
 
             if (alpha)
@@ -83,6 +96,7 @@ namespace Projet_SAE_24_Stargate
                 ordreTri = "gravite ASC";
             }
 
+            // Construction Des Conditions De Filtre Selon La Sélection Du Combobox DataBaz
             List<string> conditions = new List<string>();
 
             if (cboData.SelectedItem != null && cboData.SelectedIndex != 0)
@@ -99,10 +113,12 @@ namespace Projet_SAE_24_Stargate
 
             string filtre = string.Join(" AND ", conditions);
 
+            // Parcours De Chaque Planète Filtrée Et Triée
             foreach (DataRow row in MesDatas.DsGlobal.Tables["Planete"].Select(filtre, ordreTri))
             {
                 string nom = row["nom"].ToString();
 
+                // Filtrage Par Présence Ou Absence D'Espèces Sur La Planète
                 if (cboAliens.SelectedItem != null && cboAliens.SelectedIndex != 0)
                 {
                     bool aDesAliens = MesDatas.DsGlobal.Tables["Habiter"].Select("nomPlanete = '" + nom.Replace("'", "''") + "'").Length > 0;
@@ -117,19 +133,23 @@ namespace Projet_SAE_24_Stargate
                     }
                 }
 
+                // Chargement De L'Image De La Planète Depuis Les Ressources
                 Image image = (Image)Properties.Resources.ResourceManager.GetObject(nom);
 
+                // Image Par Défaut Si Aucune Image N'Est Trouvée
                 if (image == null)
                 {
                     image = Properties.Resources.PlanetNotFound;
                 }
 
+                // Création Et Ajout De La Carte Visuelle De La Planète Dans Le Panneau
                 ucPlanetes ucPlanete = new ucPlanetes(nom, image);
                 ucPlanete.Click += new EventHandler(ucPlanete_Click);
                 flpPlanet.Controls.Add(ucPlanete);
             }
         }
 
+        // Quand Le Filtre DataBaz Change : Rafraîchit L'Affichage Des Planètes
         private void cboData_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (!enUpdate)
@@ -140,6 +160,7 @@ namespace Projet_SAE_24_Stargate
             }
         }
 
+        // Quand Le Filtre Espèces Change : Rafraîchit L'Affichage Des Planètes
         private void cboAliens_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (!enUpdate)
@@ -150,6 +171,7 @@ namespace Projet_SAE_24_Stargate
             }
         }
 
+        // Quand Le Tri Alphabétique Est Coché : Décoche Les Autres Tris Et Rafraîchit
         private void chkTriAlpha_CheckedChanged(object sender, EventArgs e)
         {
             if (!enUpdate)
@@ -165,6 +187,7 @@ namespace Projet_SAE_24_Stargate
             }
         }
 
+        // Quand Le Tri Par Température Est Coché : Décoche Les Autres Tris Et Rafraîchit
         private void ChkTriTemp_CheckedChanged(object sender, EventArgs e)
         {
             if (!enUpdate)
@@ -180,6 +203,7 @@ namespace Projet_SAE_24_Stargate
             }
         }
 
+        // Quand Le Tri Par Gravité Est Coché : Décoche Les Autres Tris Et Rafraîchit
         private void chkTriPes_CheckedChanged(object sender, EventArgs e)
         {
             if (!enUpdate)
@@ -195,8 +219,10 @@ namespace Projet_SAE_24_Stargate
             }
         }
 
+        // Quand Une Planète Est Cliquée : Affiche Ses Informations Et Les Espèces Qui L'Habitent
         private void ucPlanete_Click(object sender, EventArgs e)
         {
+            // Récupération Du Nom De La Planète Cliquée
             ucPlanetes planeteClicked = sender as ucPlanetes;
 
             if (planeteClicked != null)
@@ -204,6 +230,7 @@ namespace Projet_SAE_24_Stargate
                 planetSelect = planeteClicked.nomPlanete;
             }
 
+            // Affichage De La Température Ou "Inconnu" Si Absente
             if (MesDatas.DsGlobal.Tables["Planete"].Select("nom = '" + planetSelect + "'")[0]["temperature"] != DBNull.Value)
             {
                 lblTemp.Text = MesDatas.DsGlobal.Tables["Planete"].Select("nom = '" + planetSelect + "'")[0]["temperature"].ToString() + "°C";
@@ -213,6 +240,7 @@ namespace Projet_SAE_24_Stargate
                 lblTemp.Text = "Inconnu";
             }
 
+            // Affichage De La Gravité Ou "Inconnu" Si Absente
             if (MesDatas.DsGlobal.Tables["Planete"].Select("nom = '" + planetSelect + "'")[0]["gravite"] != DBNull.Value)
             {
                 lblPes.Text = MesDatas.DsGlobal.Tables["Planete"].Select("nom = '" + planetSelect + "'")[0]["gravite"].ToString() + "g";
@@ -222,6 +250,7 @@ namespace Projet_SAE_24_Stargate
                 lblPes.Text = "Inconnu";
             }
 
+            // Affichage De La Disponibilité De La DataBaz Sur La Planète
             if (MesDatas.DsGlobal.Tables["Planete"].Select("nom = '" + planetSelect + "'")[0]["dataBazON"] != DBNull.Value && Convert.ToInt32(MesDatas.DsGlobal.Tables["Planete"].Select("nom = '" + planetSelect + "'")[0]["dataBazON"]) == 1)
             {
                 lblDatabaz.Text = "Disponible";
@@ -231,16 +260,20 @@ namespace Projet_SAE_24_Stargate
                 lblDatabaz.Text = "Indisponible";
             }
 
+            // Nettoyage Du Panneau Des Espèces Avant Affichage
             flpMonstres.Controls.Clear();
 
+            // Récupération De Toutes Les Espèces Habitant Cette Planète
             DataRow[] habitations = MesDatas.DsGlobal.Tables["Habiter"].Select("nomPlanete = '" + planetSelect + "'");
 
             if (habitations.Length > 0)
             {
+                // Parcours De Chaque Espèce Présente Sur La Planète
                 foreach (DataRow hab in habitations)
                 {
                     string idEspece = hab["idEspece"].ToString();
 
+                    // Vérification Si L'Espèce Est Alliée Ou Ennemie
                     DataRow[] estAllie = MesDatas.DsGlobal.Tables["Allie"].Select("idEspece = " + idEspece);
                     DataRow[] estEnnemi = MesDatas.DsGlobal.Tables["Ennemi"].Select("idEspece = " + idEspece);
 
@@ -251,24 +284,30 @@ namespace Projet_SAE_24_Stargate
                         String nom = espece[0]["nom"].ToString();
                         String couleur = espece[0]["couleur"].ToString();
 
+                        // Traduction De La Couleur En Anglais Pour L'Affichage
                         if (dicoTrad.ContainsKey(couleur))
                         {
                             couleur = dicoTrad[couleur];
                         }
 
+                        // Chargement De L'Image De L'Espèce Depuis Les Ressources
                         Image image = (Image)Properties.Resources.ResourceManager.GetObject(nom);
 
                         string insArme = "";
                         string attitude = "";
+                        // Préparation Du Texte Du Pourcentage De Population
                         string pourcentage = " % De Population : ";
+                        // -1 = Neutre, 0 = Allié, 1 = Ennemi
                         int allie_enemy = -1;
 
+                        // Récupération Des Informations Spécifiques Aux Alliés
                         if (estAllie.Length > 0)
                         {
                             insArme = estAllie[0]["instrumentMusique"].ToString();
                             attitude = estAllie[0]["degreBienveillance"].ToString();
                             allie_enemy = 0;
                         }
+                        // Récupération Des Informations Spécifiques Aux Ennemis
                         else if (estEnnemi.Length > 0)
                         {
                             insArme = estEnnemi[0]["typeArme"].ToString();
@@ -276,8 +315,10 @@ namespace Projet_SAE_24_Stargate
                             allie_enemy = 1;
                         }
 
+                        // Ajout Du Pourcentage De Population De L'Espèce Sur La Planète
                         pourcentage += hab["pourcentage"].ToString() + " %";
 
+                        // Création Et Ajout De La Carte Visuelle De L'Espèce Dans Le Panneau
                         ucRaces ucRace = new ucRaces(nom, planetSelect, couleur, image, insArme, attitude, pourcentage, allie_enemy);
                         flpMonstres.Controls.Add(ucRace);
                     }
@@ -285,21 +326,26 @@ namespace Projet_SAE_24_Stargate
             }
             else
             {
+                // Affichage D'Un Message Si Aucune Espèce N'Habite La Planète
                 Label lbl = new Label();
                 lbl.Text = "AUCUNE ESPÈCE";
                 lbl.Font = new Font("Arial", 64, FontStyle.Bold);
                 lbl.ForeColor = Color.Red;
-                lbl.AutoSize = true;
+                lbl.AutoSize = false;
+                lbl.Size = flpMonstres.Size;
+                lbl.TextAlign = ContentAlignment.MiddleCenter;
 
                 flpMonstres.Controls.Add(lbl);
             }
         }
 
+        // Clic Sur L'Image De Fermeture : Ferme Le Formulaire
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        // Bouton Réinitialiser : Remet Tous Les Filtres Et Tris À Zéro
         private void button1_Click(object sender, EventArgs e)
         {
             cboAliens.SelectedIndex = 0;
